2cdd58c0ca98341d48f58a0217ef5893659ed18a
Fabiana Tavares <fabi.tavares1@gmail.com>
2021-04-04 15:43:26 -0300

    Refactory da classe calculadora, evitando repetição de codigo.

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,4 +1,64 @@
 var Calculadora = {
+  ADICAO: '+',
+  SUBTRACAO: '-',
+  DIVISAO: '/',
+  MULTIPLICACAO: '*',
+
+  adicionar: function(num1, num2){
+    return Calculadora.calcular(num1, num2, Calculadora.ADICAO);
+  },
+
+  subtrair: function(num1, num2){
+    return Calculadora.calcular(num1, num2, Calculadora.SUBTRACAO);
+  },
+
+  dividir: function(num1, num2){
+    if(num2 == 0){
+      return 'Erro';
+    }
+    return Calculadora.calcular(num1, num2, Calculadora.DIVISAO);
+  },
+
+  multiplicar: function(num1, num2){
+    return Calculadora.calcular(num1, num2, Calculadora.MULTIPLICACAO);
+  },
+
+  calcular: function(num1, num2, operacao){
+    var resultado = 0;
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    if(isNaN(num1) || isNaN(num2)){
+      return 0;
+    }
+
+    switch(operacao){
+      case Calculadora.ADICAO:
+        resultado = num1 + num2;
+        break;
+      case Calculadora.SUBTRACAO:
+        resultado = num1 - num2;
+        break;
+      case Calculadora.DIVISAO:
+        resultado = num1 / num2;
+        break;
+      case Calculadora.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      default:
+        resultado = 0;
+        break;
+    }
+    return resultado;
+  }
+};
+
+if ( typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
+  module.exports = Calculadora;
+}
+
+/* // Codigo antigo, antes da refatoração
+var Calculadora = {
   adicionar: function(num1, num2){
 
     if(isNaN(num1) || isNaN(num2)){ // item 4 e 5
@@ -7,6 +67,7 @@ var Calculadora = {
 
     /* num1 = parseInt(num1);
     num2 = parseInt(num2); */
+    /*
     num1 = parseFloat(num1);
     num2 = parseFloat(num2);
 
@@ -41,3 +102,4 @@ var Calculadora = {
 if ( typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
   module.exports = Calculadora;
 }
+*/
